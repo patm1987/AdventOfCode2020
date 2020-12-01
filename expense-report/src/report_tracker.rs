584a2cbaf211a6e384f4503b625f318979341957
Patrick Martin <pux1987@gmail.com>
2020-12-01 09:00:19 -0700

    Starting work, comitting for now

--- expense-report/src/report_tracker.rs
+++ expense-report/src/report_tracker.rs
@@ -1,8 +1,9 @@
 pub struct ReportTracker {
+    target_sum: u32,
 }
 
-pub fn build_report_tracker() -> ReportTracker {
-    ReportTracker{}
+pub fn build_report_tracker(target_sum: u32) -> ReportTracker {
+    ReportTracker { target_sum }
 }
 
 impl ReportTracker {
@@ -17,9 +18,16 @@ mod tests {
 
     #[test]
     fn test_add_returns_empty() {
-        let report = build_report_tracker();
+        let report = build_report_tracker(2);
         let (matched, total) = report.add_expense(1337);
         assert_eq!(matched, false);
         assert_eq!(total, 0);
+    }
+
+    #[test]
+    fn test_finds_match() {
+        let report = build_report_tracker(2);
+        assert_eq!((false, 0), report.add_expense(1));
+        assert_eq!((true, 1), report.add_expense(1));
     }
 }
