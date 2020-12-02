9421083a3bbac9ffe8aceaa5d16bee7acae2de98
Patrick Martin <pux1987@gmail.com>
2020-12-01 19:24:58 -0700

    It works with two numbers

--- expense-report/src/report_tracker.rs
+++ expense-report/src/report_tracker.rs
@@ -1,14 +1,30 @@
 pub struct ReportTracker {
-    target_sum: u32,
+    target_sum: i32,
+    looking_for: Option<i32>,
 }
 
-pub fn build_report_tracker(target_sum: u32) -> ReportTracker {
-    ReportTracker { target_sum }
+pub fn build_report_tracker(target_sum: i32) -> ReportTracker {
+    ReportTracker {
+        target_sum,
+        looking_for: None,
+    }
 }
 
 impl ReportTracker {
-    pub fn add_expense(&self, value: u32) -> (bool, u32) {
-        (false, 0)
+    pub fn add_expense(&mut self, value: i32) -> (bool, i32) {
+        match self.looking_for {
+            Some(x) => {
+                if value == x {
+                    return (true, (self.target_sum - x) * x);
+                } else {
+                    return (false, 0);
+                }
+            }
+            None => {
+                self.looking_for = Some(self.target_sum - value);
+                return (false, 0);
+            }
+        }
     }
 }
 
@@ -18,7 +34,7 @@ mod tests {
 
     #[test]
     fn test_add_returns_empty() {
-        let report = build_report_tracker(2);
+        let mut report = build_report_tracker(2);
         let (matched, total) = report.add_expense(1337);
         assert_eq!(matched, false);
         assert_eq!(total, 0);
@@ -26,8 +42,8 @@ mod tests {
 
     #[test]
     fn test_finds_match() {
-        let report = build_report_tracker(2);
+        let mut report = build_report_tracker(3);
         assert_eq!((false, 0), report.add_expense(1));
-        assert_eq!((true, 1), report.add_expense(1));
+        assert_eq!((true, 2), report.add_expense(2));
     }
 }
