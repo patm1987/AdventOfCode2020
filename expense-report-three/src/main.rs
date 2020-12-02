d8a6831e90d5c74a23871d5fefebcce93dccb4f4
Patrick Martin <patrick.martin@hey.com>
2020-12-01 21:27:32 -0700

    I have the basics in, now I can start building the algorithm

--- expense-report-three/src/main.rs
+++ expense-report-three/src/main.rs
@@ -3,7 +3,7 @@ fn main() {
 }
 
 fn find_multiple_of_three(input: Vec<i32>, target: i32) -> Option<i32> {
-    None
+    if input.len() < 3 { None } else if (input[0] + input[1] + input[2] == target) { Some(input[0] * input[1] * input[2]) } else { None }
 }
 
 #[cfg(test)]
@@ -19,5 +19,11 @@ mod tests {
     fn multiplies_three() {
         let values = vec![2, 3, 4];
         assert_eq!(Some(24), find_multiple_of_three(values, 9))
+    }
+
+    #[test]
+    fn finds_none_if_sum_isnt_target() {
+        let values = vec![2, 3, 4];
+        assert_eq!(None, find_multiple_of_three(values, 10))
     }
 }
