2f9e4bc2f4a0c2e9b4606b1c6ee3acee3531f847
Patrick Martin <pux1987@gmail.com>
2020-12-01 19:39:22 -0700

    Now finding the correct over time

--- expense-report/src/report_tracker.rs
+++ expense-report/src/report_tracker.rs
@@ -1,32 +1,25 @@
+use std::collections::HashSet;
+
 pub struct ReportTracker {
     target_sum: i32,
-    looking_for: Option<i32>,
+    looking_for: HashSet<i32>,
 }
 
 pub fn build_report_tracker(target_sum: i32) -> ReportTracker {
     ReportTracker {
         target_sum,
-        looking_for: None,
+        looking_for: HashSet::new(),
     }
 }
 
 impl ReportTracker {
     pub fn add_expense(&mut self, value: i32) -> (bool, i32) {
-        match self.looking_for {
-            Some(x) => {
-                if value == x {
-                    return (true, (self.target_sum - x) * x);
-                } else {
-                    return (false, 0);
-                }
-            }
-            None => {
-                if value < self.target_sum {
-                self.looking_for = Some(self.target_sum - value);
-                }
-                return (false, 0);
-            }
+        if self.looking_for.contains(&value) {
+            return (true, (self.target_sum - value) * value);
+        } else if value < self.target_sum {
+            self.looking_for.insert(self.target_sum - value);
         }
+        (false, 0)
     }
 }
 
@@ -55,5 +48,13 @@ mod tests {
         assert_eq!((false, 0), report.add_expense(5));
         assert_eq!((false, 0), report.add_expense(1));
         assert_eq!((true, 2), report.add_expense(2));
+    }
+
+    #[test]
+    fn test_finds_match_if_first_misleading() {
+        let mut report = build_report_tracker(4);
+        assert_eq!((false, 0), report.add_expense(2));
+        assert_eq!((false, 0), report.add_expense(3));
+        assert_eq!((true, 3), report.add_expense(1));
     }
 }
