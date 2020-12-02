c790b1271547fd4c1321e7150380b56dbe32aa6f
Patrick Martin <pux1987@gmail.com>
2020-12-01 21:03:26 -0700

    working on building out 2nd algorithm in tests - out of battery

--- expense-report-three/src/main.rs
+++ expense-report-three/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn find_multiple_of_three(input: Vec<i32>, target: i32) -> Option<i32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn none_for_empty() {
+        assert_eq!(None, find_multiple_of_three(Vec::new(), 1))
+    }
+
+    #[test]
+    fn multiplies_three() {
+        let values = vec![2, 3, 4];
+        assert_eq!(Some(24), find_multiple_of_three(values, 9))
+    }
+}
