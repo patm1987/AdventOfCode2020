501911db25f285c17e5fd4833a1654fff8cbd4f6
Patrick Martin <patrick.martin@hey.com>
2020-12-02 23:50:58 -0700

    reading stuff in, but it says hit 28 trees and that wasn't right

--- toboggan-trajectory/src/main.rs
+++ toboggan-trajectory/src/main.rs
@@ -1,9 +1,20 @@
 use crate::map::build_map;
+use std::{env, fs};
 
 mod map;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Please specify your input");
+        return;
+    }
+
+    let file: &str = &args[1];
+    let input_map = fs::read_to_string(file).expect("Failed to read input");
+    let count = trees_hit(1, 3, &input_map);
+
+    println!("hit {} trees", count);
 }
 
 fn trees_hit(dx: i32, dy: i32, map: &str) -> i32 {
