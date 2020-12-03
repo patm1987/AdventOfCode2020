88798b86924a1033fc93f88b4a93e62b94a580c6
Patrick Martin <patrick.martin@hey.com>
2020-12-02 22:34:05 -0700

    added the desired output

--- toboggan-trajectory/src/main.rs
+++ toboggan-trajectory/src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn trees_hit(dx: i32, dy: i32, map: &str) -> i32{
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn puzzle_validation() {
+        let input = "..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#";
+
+        assert_eq!(7, trees_hit(3, 1, input))
+    }
+}
