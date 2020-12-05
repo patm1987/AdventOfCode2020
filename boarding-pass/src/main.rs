fd5a46d04c0c089ae95fb535c90d7608373eeb06
Patrick Martin <pux1987@gmail.com>
2020-12-04 22:16:18 -0700

    Created empty project with basic input

---
+++ boarding-pass/src/main.rs
@@ -0,0 +1,18 @@
+const ROWS: (i32, i32) = (0, 127);
+const COLS: (i32,i32) = (0,7);
+
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /*
+     * Sample:
+     * BFFFBBFRRR: row 70, column 7, seat ID 567.
+     * FFFBBBFRRR: row 14, column 7, seat ID 119.
+     * BBFFBBFRLL: row 102, column 4, seat ID 820.
+     */
+}
