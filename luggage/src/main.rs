d5dccf80305bcdaec2818e8b102ea25d23fe43e8
Patrick Martin <patrick.martin@hey.com>
2020-12-06 23:33:59 -0700

    I'm now parsing the bags

--- luggage/src/main.rs
+++ luggage/src/main.rs
@@ -1,3 +1,5 @@
+use regex::{Regex, Captures, Match};
+
 fn main() {
     println!("Hello, world!");
 }
@@ -15,8 +17,43 @@ impl Bag {
             bags: bags.iter().map(|x| (x.0, String::from(x.1))).collect(),
         }
     }
-    fn parse(input: String) -> Bag {
-        Bag { name: String::from(""), bags: Vec::new() }
+    fn parse(input: String) -> Result<Bag, &'static str> {
+        let re = Regex::new(r"^(.+) bags{0,1} contain ((\d+) (.+) bags{0,1}, )*((\d+) (.+) bags{0,1})\.$").unwrap();
+        match re.captures(&input) {
+            Some(captures) => {
+                println!("Captures: {:?}", captures);
+                let mut iter = captures.iter();
+                iter.next(); // skip the full string
+                let mut bag = Bag {
+                    name: iter.next().unwrap().unwrap().as_str().to_string(),
+                    bags: Vec::new(),
+                };
+                loop {
+                    // skip one for the whole group
+                    match iter.next() {
+                        None => { break; }
+                        Some(_) => {}
+                    }
+
+                    match iter.next() {
+                        Some(count_str) => {
+                            let count: i32 = count_str.unwrap().as_str().parse::<i32>().unwrap();
+                            match iter.next() {
+                                Some(name) => {
+                                    bag.bags.push((count, name.unwrap().as_str().to_string()));
+                                }
+                                None => { break; }
+                            }
+                        }
+                        None => { break; }
+                    }
+                }
+                Ok(bag)
+            }
+            None => {
+                Err("Failed to parse input")
+            }
+        }
     }
 }
 
@@ -38,6 +75,6 @@ dotted black bags contain no other bags.";
     fn test_parses_bag() {
         let sample_input = "light red bags contain 1 bright white bag, 2 muted yellow bags.";
         let expected = Bag::new("light red", &[(1, "bright white"), (2, "muted yellow")]);
-        assert_eq!(expected, Bag::parse(String::from(sample_input)));
+        assert_eq!(expected, Bag::parse(String::from(sample_input)).unwrap());
     }
 }
