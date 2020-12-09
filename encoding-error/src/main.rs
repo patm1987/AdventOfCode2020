4516729ce3812ec91a010abce36a2f6eda359f21
Patrick Martin <patrick.martin@hey.com>
2020-12-09 08:31:03 -0700

    Can parse the sample input

--- encoding-error/src/main.rs
+++ encoding-error/src/main.rs
@@ -1,3 +1,61 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn parse_numbers(input: &str) -> Vec<i32> {
+    input.lines().filter_map(|x| x.parse::<i32>().ok()).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const SAMPLE_INPUT: &str = "35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576";
+
+    #[test]
+    fn test_parses_input() {
+        let expected = vec![
+            35,
+            20,
+            15,
+            25,
+            47,
+            40,
+            62,
+            55,
+            65,
+            95,
+            102,
+            117,
+            150,
+            182,
+            127,
+            219,
+            299,
+            277,
+            309,
+            576
+        ];
+
+        assert_eq!(parse_numbers(SAMPLE_INPUT), expected);
+    }
+}
