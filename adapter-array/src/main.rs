fb7fcb0609fbe9beba96fca57f71435cf586dd49
Patrick Martin <patrick.martin@hey.com>
2020-12-11 09:17:22 -0700

    Trying new counting idea

--- adapter-array/src/main.rs
+++ adapter-array/src/main.rs
@@ -31,6 +31,10 @@ fn enumerate_differences(input: &mut Vec<i32>) -> (i32, i32) {
     input.windows(2).fold(start, |acc, x| accumulate_window(acc, x))
 }
 
+fn to_derivative(input: &Vec<i32>) -> Vec<i32> {
+    input.as_slice().windows(2).map(|x| x[1] - x[0]).collect()
+}
+
 fn parse_input(input: &str) -> Vec<i32> {
     let mut output: Vec<i32> = input.trim().lines().filter_map(|line| line.parse::<i32>().ok()).collect();
     output.sort();
@@ -58,6 +62,80 @@ fn validate_range<'a, T>(range: T, start: i32, end: i32) -> bool
         None => { false }
     }
 }
+
+fn count_permutations(input: &Vec<i32>) -> i32 {
+    let derivative = to_derivative(input);
+    println!("I: {:?}", input);
+    println!("D: {:?}", derivative);
+    let mut iter = derivative.iter();
+    let mut start = 3;
+    let mut end = 3;
+    let mut at_end = false;
+    let mut sum = 1; // the current arrangement is 1 permutation
+    while !at_end {
+        // scan for 1's
+        let mut count_1 = 0;
+        while !at_end {
+            match iter.next() {
+                None => {
+                    end = 3;
+                    at_end = true
+                }
+                Some(x) => {
+                    if *x != 1 {
+                        start = *x;
+                    } else {
+                        count_1 += 1;
+                        break;
+                    }
+                }
+            }
+        }
+
+        // count 1's
+        while !at_end {
+            match iter.next() {
+                None => {
+                    end = 3;
+                    at_end = true;
+                }
+                Some(x) => {
+                    if *x != 1 {
+                        end = *x;
+                        break;
+                    } else {
+                        count_1 += 1;
+                    }
+                }
+            }
+        }
+
+        println!("Counting {} 1's", count_1);
+
+        match count_1 {
+            0 | 1 => { println!("+0") } // nothing in the middle, can't add
+            2 => {
+                println!("+1");
+                sum += 1;
+                if start == 2 && end == 2 {
+                    sum += 1;
+                }
+            } // can remove one from the middle
+            x => {
+                println!("+{}", (x - 2) * 3);
+                sum += (x - 2) * 3;
+                if start == 2 && end == 2 {
+                    sum += 2;
+                } else if start == 2 || end == 2 {
+                    sum += 1;
+                }
+            }
+        }
+    }
+    // TODO: start at 0
+    sum
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -95,6 +173,18 @@ mod tests {
 10
 3";
 
+    const SMALL_SAMPLE: &str = "16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4";
+
     #[test]
     fn test_passes_sample() {
         assert_eq!((22, 10), enumerate_differences(&mut parse_input(SAMPLE_INPUT)))
@@ -118,5 +208,15 @@ mod tests {
     #[test]
     fn checks_end_for_validation() {
         assert!(!validate_range(&[1, 2, 3], 0, 7))
+    }
+
+    #[test]
+    fn validates_small_sample() {
+        assert_eq!(8, count_permutations(&parse_input(SMALL_SAMPLE)))
+    }
+
+    #[test]
+    fn validates_sample_2() {
+        assert_eq!(19208, count_permutations(&parse_input(SAMPLE_INPUT)))
     }
 }
